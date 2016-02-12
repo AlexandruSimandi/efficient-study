c26e5688f0eae7c1f8973460ba28b3d4077550e5
AlexandruSimandi <alex_simi_jr@yahoo.com>
2016-02-12 15:56:47 +0200

    study timer working

--- js/main.js
+++ js/main.js
@@ -1,4 +1,118 @@
 $(document).ready(function() {
-	$(".button-collapse").sideNav();
-	$('select').material_select();
+//	$(".button-collapse").sideNav();
+//	$('select').material_select();
+	loadEvents();
 });
+
+function loadEvents(){
+	var alreadyFired = false;
+	var interval;
+	$('#setTimer').click(function(){
+		if(alreadyFired){
+			clearInterval(interval);
+		}
+		var studyTime = parseInt(document.getElementById('studyTime').value);
+		var pauseTime = parseInt(document.getElementById('pauseTime').value);
+
+		var hours = Math.floor(studyTime / 60);
+		var minutes = studyTime % 60;
+		var seconds = 0;
+
+		document.getElementById('seconds').innerHTML = '0s';
+		document.getElementById('minutes').innerHTML = minutes + 'm';
+		document.getElementById('hours').innerHTML = hours + 'h';
+
+		interval = setInterval(function(){
+			if(seconds == 0 ){
+				if(minutes == 0){
+					if(hours == 0){
+						//switch to pause timer
+					} else {
+						hours--;
+						minutes = 59;
+						seconds = 59;
+					}
+					updateTime('hours', hours);
+				} else {
+					minutes--;
+					seconds = 59;
+				}
+				updateTime('minutes', minutes);
+			} else {
+				seconds--;
+			}
+			updateTime('seconds', seconds);
+		},1000);
+
+		alreadyFired = true;
+
+	});
+
+	//////////////////////////////
+
+	var hidden, visibilityChange;
+	if (typeof document.hidden !== "undefined") { // Opera 12.10 and Firefox 18 and later support
+		hidden = "hidden";
+		visibilityChange = "visibilitychange";
+	} else if (typeof document.mozHidden !== "undefined") {
+		hidden = "mozHidden";
+		visibilityChange = "mozvisibilitychange";
+	} else if (typeof document.msHidden !== "undefined") {
+		hidden = "msHidden";
+		visibilityChange = "msvisibilitychange";
+	} else if (typeof document.webkitHidden !== "undefined") {
+		hidden = "webkitHidden";
+		visibilityChange = "webkitvisibilitychange";
+	}
+
+//	var hoursH1 = document.getElementById("hours");
+
+	// If the page is hidden, pause the video;
+	// if the page is shown, play the video
+	function handleVisibilityChange() {
+		if (document[hidden]) {
+//			hoursH1.innerHTML = 'hey!';
+		} else {
+//			hoursH1.innerHTML = 'oh you are back';
+			var secondsH1 = $('#seconds');
+			secondsH1.stop(true, true);
+			secondsH1.animate({
+				marginTop: 31.5,
+				opacity: 1.0
+			}, 1, function(){});
+		}
+	}
+
+	// Warn if the browser doesn't support addEventListener or the Page Visibility API
+	if (typeof document.addEventListener === "undefined" ||
+		typeof document[hidden] === "undefined") {
+		alert("This demo requires a browser, such as Google Chrome or Firefox, that supports the Page Visibility API.");
+	} else {
+		// Handle page visibility change
+		document.addEventListener(visibilityChange, handleVisibilityChange, false);
+
+	}
+
+	////////////////////
+}
+
+function updateTime(element, value) {
+	var secondsH1 = document.getElementById(element);
+	$(secondsH1).animate({
+		marginTop: -21.5,
+		opacity: 0.0
+	}, 200, 'swing', function(){});
+
+	setTimeout(function(){
+		secondsH1.innerHTML = value + element.charAt(0);
+		$(secondsH1).animate({
+			marginTop: 81.5,
+			opacity: 0.5
+		}, 1, 'linear', function(){});
+		$(secondsH1).animate({
+			marginTop: 31.5,
+			opacity: 1.0
+		}, 200, 'swing', function(){});
+	}, 200);
+}
+
